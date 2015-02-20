306cf5d7a7ad01094d7ffc82e2a55144f2d05ed2
Christopher Tam <ohgodtamit@gmail.com>
2015-02-19 22:08:57 -0500

    Add new Material Design views

--- ShopWithFriends/app/src/main/java/www/shopwithfriends/com/shopwithfriends/FriendListActivity.java
+++ ShopWithFriends/app/src/main/java/www/shopwithfriends/com/shopwithfriends/FriendListActivity.java
@@ -1,12 +1,8 @@
 package www.shopwithfriends.com.shopwithfriends;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,9 +18,9 @@ public class FriendListActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friend_list);
+        setContentView(R.layout.activity_friends_list);
 
-        final ListView listview = (ListView) findViewById(R.id.listview);
+        final ListView listview = (ListView) findViewById(R.id.friends_list);
         String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
                 "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
                 "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
