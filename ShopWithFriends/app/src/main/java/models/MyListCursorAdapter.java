c060e0e91ffacc616550f71e13b62b0531262062
Oleg Filatov <ofilatov3@gatech.edu>
2015-02-20 02:15:12 -0500

    aaa

---
+++ ShopWithFriends/app/src/main/java/models/MyListCursorAdapter.java
@@ -0,0 +1,46 @@
+package models;
+
+/**
+ * Created by filoleg on 2/20/15.
+ */
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+import www.shopwithfriends.com.shopwithfriends.R;
+
+
+public class MyListCursorAdapter extends CursorRecyclerViewAdapter<MyListCursorAdapter.ViewHolder>{
+
+    public MyListCursorAdapter(Context context,Cursor cursor){
+        super(context,cursor);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(View view) {
+            super(view);
+            //mTextView = view.findViewById(R.id.text);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_friends_list, parent, false);
+        ViewHolder vh = new ViewHolder(itemView);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, Cursor cursor) {
+        User myListItem = User.fromCursor(cursor);
+        viewHolder.mTextView.setText(myListItem.getUserName());
+    }
+}
