aedae69a52987a2da06adba62172a131fc3c45ac
Christopher Tam <ohgodtamit@gmail.com>
2015-02-20 11:14:47 -0500

    Moved 'models' package to com.theratio.utilities

--- ShopWithFriends/app/src/main/java/com/theratio/utilities/User.java
+++ ShopWithFriends/app/src/main/java/com/theratio/utilities/User.java
@@ -1,4 +1,4 @@
-package models;
+package com.theratio.utilities;
 
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
--- ShopWithFriends/app/src/main/java/com/theratio/utilities/Utility.java
+++ ShopWithFriends/app/src/main/java/com/theratio/utilities/Utility.java
@@ -1,4 +1,4 @@
-package models;
+package com.theratio.utilities;
 
 import android.app.Activity;
 import android.app.AlertDialog;
